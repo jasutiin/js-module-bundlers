8cf0bd5344f4720f9539fb6b9a27dea81523e067
justine <justinemangaliman@gmail.com>
2025-11-07 12:57:07 -0700

    trying to implement live reload
    it isn't working. that eventsource code is found in the docs, but it
    doesn't specify where to put it. maybe it has something to do with how i
    put it in a .jsx file and not .js because the docs say .js?

--- esbuild/src/index.jsx
+++ esbuild/src/index.jsx
@@ -1,11 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react'; // need this import even though it's not explicitly used
 
+new EventSource('/esbuild').addEventListener('change', () => location.reload());
+
 function App() {
   return (
     <div>
       <h1>Hello from esbuild + React!</h1>
-      <p>This is a simple React app without CRA.</p>
+      <p>This is a simple React app wihout CRA.</p>
     </div>
   );
 }
